c48f749e9805c7328cfac5da6653d6489f9a9bbc
hampus <haslun16@edu.vilhelmina.se>
2017-05-02 19:36:12 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const notifier = require('node-notifier');
+const rantscript = require('rantscript');
+
+// Object
+notifier.notify({
+  'title': 'My notification',
+  'message': 'Hello, there!'
+});
